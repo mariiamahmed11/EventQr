efa03e5089df766c4369ec632eb81261a573e343
mariam <mariiamahmed11@hotmail.com>
2023-06-13 13:48:47 +0300

    Initial Commit

---
+++ EventQr/EventQrApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventQrApp.swift
+//  EventQr
+//
+//  Created by user on 13/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct EventQrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
